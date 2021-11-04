d4b2a6ef00896a55a95b11ab7da5b69fe0018241
SungHwan-Park <shwpark612@gmail.com>
2021-11-03 20:25:51 -0700

    add table of feeds

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,20 +1,56 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  Table, 
+  TableContainer,
+  TableCell,
+  TableBody,
+  TableHead,
+  TableRow
+ } from '@material-ui/core';
 
 import { IFeed } from '@freshworks/shared';
 
 import styles from './app.module.scss';
 
 export function App() {
-  const [ message, setMessage ] = useState<IFeed>();
+  const [ feeds, setFeeds ] = useState<IFeed[]>([]);
   useEffect(() => {
-    fetch('/api/feeds').then(res => res.json()).then(result => {
-      setMessage(result)
+    axios.get('/api/feeds').then(result => {
+      setFeeds(result.data)
     })
   },[])
   return (
     <div className={styles.app}>
       <h1>How people feed ducks!</h1>
-      <div>{ JSON.stringify(message) }</div>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Farmer</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Count</TableCell>
+              <TableCell>Quantity</TableCell>
+              <TableCell>Food</TableCell>
+              <TableCell>Place</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {feeds.map(feed => (
+              <TableRow key={feed.id}>
+                <TableCell>{feed.id}</TableCell>
+                <TableCell>{feed.farmer}</TableCell>
+                <TableCell>{feed.date}</TableCell>
+                <TableCell>{feed.count}</TableCell>
+                <TableCell>{feed.quantity}</TableCell>
+                <TableCell>{feed.food}</TableCell>
+                <TableCell>{feed.place}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
